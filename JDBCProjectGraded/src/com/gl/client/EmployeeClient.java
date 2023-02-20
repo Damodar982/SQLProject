fb30a907e2c22e31ee26f44d1a942d0b3aff6c9a
Damodar982 <118039665+Damodar982@users.noreply.github.com>
2023-02-20 09:46:59 +0530

    Add files via upload

---
+++ JDBCProjectGraded/src/com/gl/client/EmployeeClient.java
@@ -0,0 +1,10 @@
+package com.gl.client;
+
+public class EmployeeClient {
+
+	public static void main(String[] args) {
+		EmployeeDBConnection empDBConnection=new EmployeeDBConnection();
+		empDBConnection.showMenu();
+	}
+
+}
